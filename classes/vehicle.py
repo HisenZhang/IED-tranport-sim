80606335706b8d932d9782bd49399fc7fa0bf070
Hisen Zhang <zhangz29@rpi.edu>
2020-06-02 13:52:51 -0400

    ADD load() and _updateWeight()

--- classes/vehicle.py
+++ classes/vehicle.py
@@ -1,11 +1,35 @@
 class Vehicle:
-    def __init__(self,engine,carWeight,trailerWeight,volume,load):
+    def __init__(self,engine,carWeight,trailerWeight,volume,loadList):
         self.carWeight = carWeight
+        self.weight = carWeight
         self.trailerWeight = trailerWeight
         self.engine = engine
         self.volume = volume
-        self.load = load
-        pass
+        self.loadList = loadList
+        self._updateWeight()
+
+        self.velocity = 0
+        self.power = 0
+
+    def _updateWeight(self):
+        weightSum = 0
+        for item in self.loadList:
+            weightSum += item.weight
+        self.weight = weightSum + self.carWeight
+
+    def load(self,objectList):
+        self.loadList.extend(objectList)
+        self._updateWeight()
+
+    def __str__(self):
+        retString = ""
+        for item in self.loadList:
+            retString += item.__str__()
+        return retString
+
+
+
+
 
     
 
