6de382455e716340431603818f488310cb23b185
Hisen Zhang <zhangz29@rpi.edu>
2020-06-06 14:57:26 -0400

    ADD path getDistance pathLength ADD Coordinate helper class

--- classes/path.py
+++ classes/path.py
@@ -1,2 +1,53 @@
+from simulator.constant import CONSTANT
+import math
+
+
+class Coordinate:
+    def __init__(self,latitude,longtitude):
+        self._latitude = latitude
+        self._longtitude = longtitude
+
+    def getCoordinate(self):
+        return (self._latitude,self._longtitude)
+    
+    def getLatitude(self):
+        return self._latitude
+
+    def getLongtitude(self):
+        return self._longtitude
+
+    
 class Path:
-    pass
+    def __init__(self,coordinateList):
+        self._waypointList = list()
+        self._waypointList.extend(coordinateList)
+        pass
+
+    def getDistance(self,waypoint1,waypoint2):
+
+        # distance between to points over a sphere
+        # https://en.wikipedia.org/wiki/Great-circle_distance
+        
+        lat1, lng1 = waypoint1.getCoordinate()
+        lat2, lng2 = waypoint2.getCoordinate()
+        radLat1 = math.radians(lat1)
+        radLat2 = math.radians(lat2)
+
+        a = radLat1 - radLat2
+        b = math.radians(lng1) - math.radians(lng2)
+        s = 2 * math.asin(math.sqrt(math.pow(math.sin(a/2), 2) + 
+            math.cos(radLat1) * math.cos(radLat2) * math.pow(math.sin(b/2), 2)))
+        s = s * CONSTANT['EARTH_REDIUS']
+        return s
+
+    def __len__(self):
+
+        # total length of the path
+        # consecutive element pairing  
+        waypointPairs = [(self._waypointList[i], self._waypointList[i + 1])
+            for i in range(len(self._waypointList) - 1)] 
+
+        pathLength = float()
+        for pairs in waypointPairs:
+            pathLength += self.getDistance(pairs[0],pairs[1])
+        return pathLength
