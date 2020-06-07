7f605ca39b54f224721f9d3b1e7567fd4fbd2ee3
Hisen Zhang <zhangz29@rpi.edu>
2020-06-06 23:23:28 -0400

    ADD logging & exception handle

--- simulator/core.py
+++ simulator/core.py
@@ -4,6 +4,8 @@ from classes.vehicle import Vehicle
 
 from simulator.world import World
 
+import logging
+
 
 class Simulator():
     def __init__(self, step=60000, maxIteration=0):
@@ -17,10 +19,17 @@ class Simulator():
     def update(self):
         if self._maxIteration != 0:
             if self._counter >= self._maxIteration:
+                logging.info("Max iteration reached. Stop simulation.")
                 self._stop()
+
+        try:
+            self.world.update()
+        except:
+            logging.error("Exception on updating world. Stop simulation.")
+            self._stop()
+
         self._counter += 1
         self._clock += self._step
-        self.world.update()
 
     def isRunning(self):
         return self._run
