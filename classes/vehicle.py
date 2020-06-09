4431bde5be52732ef40b9e683e322dd6e512052b
Hisen Zhang <zhangz29@rpi.edu>
2020-06-09 01:13:58 -0400

    Remove engine from param Add update()

--- classes/vehicle.py
+++ classes/vehicle.py
@@ -1,9 +1,8 @@
 class Vehicle:
-    def __init__(self, engine, carWeight, trailerWeight, volume, loadList):
+    def __init__(self, carWeight, trailerWeight, volume, loadList):
         self.carWeight = carWeight
         self.weight = carWeight
         self.trailerWeight = trailerWeight
-        self.engine = engine
         self.volume = volume
         self.loadList = loadList
         self._updateWeight()
@@ -11,12 +10,15 @@ class Vehicle:
         self.velocity = 0
         self.power = 0
 
+    def update(self, isDayTime):
+        for load in self.loadList:
+            load.update()
+
     def _updateWeight(self):
         weightSum = 0
         for item in self.loadList:
             weightSum += item.weight
-        self.weight = weightSum + self.carWeight + \
-            self.trailerWeight + self.engine.weight
+        self.weight = weightSum + self.carWeight + self.trailerWeight
 
     def load(self, objectList):
         self.loadList.extend(objectList)
