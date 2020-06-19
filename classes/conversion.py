bf5a2e51a2e817c183b88a14396163a264ce232e
Hisen Zhang <zhangz29@rpi.edu>
2020-06-19 15:19:27 -0400

    ADD EV fuel efficiency conversion

--- classes/conversion.py
+++ classes/conversion.py
@@ -1,5 +1,11 @@
 from classes.exceptions import ConversionFailure
 
 
-def MPHtoMPG(speed):
+def MPHtoMPG_Gas(speed):
     return 18  # TODO MPG as a function of speed in mph
+
+def MPHtoMPG_Electric(speed):
+    return 65
+
+def MPGtoMPKWh(mpg):
+    return 0.029669 * mpg # 1/33.705
